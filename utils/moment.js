edc33ed7a1548eb08d501809ce089a3aad288448
Alexander Paul <alexanderpaul.marinas@gmail.com>
2020-05-08 17:25:52 +0800

    JS time is hard

--- utils/moment.js
+++ utils/moment.js
@@ -5,7 +5,20 @@ const timezone = 'Asia/Manila'
 const format = 'MMMM DD YYYY h:mm:ss a'
 
 const getCurrentDate = (customFormat = format) => momentTimezone().tz(timezone).format(customFormat)
-const getTimeElapsed =  (pastDate) => moment(pastDate, format).from(moment())
+const getTimeElapsed =  (pastDate) => {
+    console.log(pastDate)
+
+    const past = momentTimezone(pastDate, format).tz(timezone, true)
+    console.log(past)
+
+    const now = moment()
+    console.log(now)
+
+    const elapsed = past.from(now)
+    console.log(elapsed)
+
+    return elapsed
+}
 
 module.exports = {
     format,
